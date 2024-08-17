8653c525e97206251bcf2cfd5952ea95278d0b8b
moonhr <moonhr153@gmail.com>
2024-08-17 18:45:10 +0900

    다이어리 작성 페이지 ui 제작

--- packages/client/src/app/diary/[slug]/page.tsx
+++ packages/client/src/app/diary/[slug]/page.tsx
@@ -1,1 +1,15 @@
-console.log('지정된 데이터 읽어올 페이지')
+import React from "react";
+import { Button, DatePicker, Textarea } from "@nextui-org/react";
+
+export default function diary() {
+  return (
+    <>
+      <form>
+        <Textarea label="제목" placeholder="제목입력칸" className="max-w-xs" />
+        <DatePicker label="날짜" className="max-w-[284px]" />
+        <Textarea label="내용" placeholder="내용입력칸" className="max-w-xs" />
+        <Button type="submit">저장</Button>
+      </form>
+    </>
+  );
+}
