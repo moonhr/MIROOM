a4468228f1a1e91bbc5de7ddacbb3a7574023cec
moonhr <moonhr153@gmail.com>
2024-08-17 18:32:36 +0900

    페이지 라우팅 홈화면 구성

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,10 +1,28 @@
 // import Image from "next/image";
+"use client";
+
 import { Button } from "@nextui-org/button";
+import { useRouter } from "next/navigation";
 
 function Home() {
+  const router = useRouter();
+
+  const handleDiaryClick = () => {
+    router.push("/diary/page");
+  };
+
+  const handleBucketListClick = () => {
+    router.push("/bucketList/page");
+  };
+
   return (
     <div>
-      <Button color="secondary">Click Me</Button>
+      <Button color="primary" variant="shadow" onClick={handleDiaryClick}>
+        다이어리
+      </Button>
+      <Button color="primary" variant="shadow" onClick={handleBucketListClick}>
+        버킷리스트
+      </Button>
     </div>
   );
 }
